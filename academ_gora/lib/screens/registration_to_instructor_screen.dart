8bfbafed69b7275043487fa113364b64ad7005f3
Крайнюков Сергей <skrajnjukov@rctahograf.ru>
2021-02-27 15:32:26 +0300

    date widget completed

--- academ_gora/lib/screens/registration_to_instructor_screen.dart
+++ academ_gora/lib/screens/registration_to_instructor_screen.dart
@@ -12,9 +12,7 @@ class RegistrationToInstructorScreen extends StatefulWidget {
 class _RegistrationToInstructorScreenState
     extends State<RegistrationToInstructorScreen> {
   int _kindOfSportSelected = -1;
-  DateTime _selectedDate = DateTime.now();
-  String _currentMonth = DateFormat.yMMM().format(DateTime.now());
-  DateTime _targetDateTime = DateTime.now();
+  DateTime _selectedDate;
 
   @override
   Widget build(BuildContext context) {
@@ -41,6 +39,7 @@ class _RegistrationToInstructorScreenState
       ),
     ));
   }
+
 
   Widget _topImages() {
     return Row(
@@ -92,7 +91,9 @@ class _RegistrationToInstructorScreenState
             which == 0 ? "ГОРНЫЕ ЛЫЖИ" : "СНОУБОРД",
             style: TextStyle(
                 fontSize: 11,
-                color: _createTextColorOfKindOfSportButton(which)),
+                color: which == _kindOfSportSelected
+                    ? Colors.white
+                    : Colors.black),
           ),
         ),
       ),
@@ -111,9 +112,7 @@ class _RegistrationToInstructorScreenState
         : "assets/registration_to_instructor/3_e1.png";
   }
 
-  Color _createTextColorOfKindOfSportButton(int which) {
-    return which == _kindOfSportSelected ? Colors.white : Colors.black;
-  }
+
 
   Widget _horizontalDivider() {
     return Container(
@@ -123,107 +122,161 @@ class _RegistrationToInstructorScreenState
     );
   }
 
+
+
   Widget _dateWidget() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
+      children: [_dateNameWidget(), _dateFieldWidget()],
+    );
+  }
+
+  Widget _dateNameWidget() {
+    return Row(
       children: [
-        Row(
-          children: [
-            Container(
-                width: 20,
-                height: 20,
-                margin: EdgeInsets.only(right: 10),
-                child:
-                    Image.asset("assets/registration_to_instructor/4_e3.png")),
-            Text(
-              "ДАТА",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            )
-          ],
-        ),
         Container(
-          width: 160,
-          height: 30,
-          margin: EdgeInsets.only(left: 90),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-          child: GestureDetector(
-            onTap: _showDateDialog,
-          ),
+            width: 20,
+            height: 20,
+            margin: EdgeInsets.only(right: 10),
+            child: Image.asset("assets/registration_to_instructor/4_e3.png")),
+        Text(
+          "ДАТА",
+          style: TextStyle(fontWeight: FontWeight.bold),
         )
       ],
     );
   }
 
+  Widget _dateFieldWidget() {
+    return Container(
+      width: 160,
+      height: 30,
+      margin: EdgeInsets.only(left: 90),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [_dateTextWidget(), _clearDateWidget()]),
+    );
+  }
+
+  Widget _dateTextWidget() {
+    return GestureDetector(
+      child: Center(
+          child: Container(
+              alignment: Alignment.center,
+              color: Colors.white,
+              height: 30,
+              width: 130,
+              child: Text(
+                _selectedDate == null
+                    ? ""
+                    : DateFormat("dd.MM.yyyy").format(_selectedDate),
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ))),
+      onTap: _showDateDialog,
+    );
+  }
+
+  Widget _clearDateWidget() {
+    return GestureDetector(
+      child: Container(
+          height: 13,
+          width: 13,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          child: Image.asset("assets/registration_to_instructor/5_e8.png")),
+      onTap: _clearDateFieldButton,
+    );
+  }
+
+  void _clearDateFieldButton() {
+    setState(() {
+      _selectedDate = null;
+    });
+  }
+
   Future<void> _showDateDialog() async {
     return showDialog<void>(
       context: context,
-      barrierDismissible: true,
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return StatefulBuilder(
             builder: (c, setState) => AlertDialog(
+                    actions: [
+                      TextButton(child: Text('OK'), onPressed: _closeDialog),
+                    ],
                     content: CalendarCarousel<Event>(
-                  locale: "ru",
-                  height: 270,
-                  todayBorderColor: Colors.transparent,
-                  todayButtonColor: Colors.transparent,
-                  todayTextStyle: TextStyle(color: Colors.blueAccent),
-                  onDayPressed: (DateTime date, List<Event> events) {
-                    setState(() => _selectedDate = date);
-                    events.forEach((event) => print(event.title));
-                  },
-                  selectedDateTime: _selectedDate,
-                  targetDateTime: _selectedDate,
-                  selectedDayTextStyle: TextStyle(color: Colors.white),
-                )));
+                      locale: "ru",
+                      width: 300,
+                      height: 270,
+                      todayBorderColor: Colors.transparent,
+                      todayButtonColor: Colors.transparent,
+                      todayTextStyle: TextStyle(color: Colors.blueAccent),
+                      onDayPressed: (DateTime date, List<Event> events) {
+                        setState(() => _selectedDate = date);
+                        events.forEach((event) => print(event.title));
+                      },
+                      selectedDateTime: _selectedDate,
+                      targetDateTime: _selectedDate,
+                      selectedDayTextStyle: TextStyle(color: Colors.white),
+                    )));
       },
     );
   }
+
+  void _closeDialog() {
+    Navigator.of(context).pop();
+    setState(() {});
+  }
+
+
 
   Widget _timeWidget() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Row(
-          children: [
-            Container(
-                width: 20,
-                height: 20,
-                margin: EdgeInsets.only(right: 10),
-                child:
-                    Image.asset("assets/registration_to_instructor/4_e4.png")),
-            Container(
-                margin: EdgeInsets.only(right: 8),
-                child: Text(
-                  "ВРЕМЯ",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ))
-          ],
-        ),
-        Container(
-          width: 80,
-          height: 30,
-          margin: EdgeInsets.only(left: 50, right: 10),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-        ),
+        _timeNameWidget(),
+        _timeField(50, 10),
         Text("-"),
-        Container(
-          width: 80,
-          height: 30,
-          margin: EdgeInsets.only(left: 10),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-        ),
+        _timeField(10, 0)
       ],
     );
   }
+
+  Widget _timeNameWidget() {
+    return Row(
+      children: [
+        Container(
+            width: 20,
+            height: 20,
+            margin: EdgeInsets.only(right: 10),
+            child: Image.asset("assets/registration_to_instructor/4_e4.png")),
+        Container(
+            margin: EdgeInsets.only(right: 8),
+            child: Text(
+              "ВРЕМЯ",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ))
+      ],
+    );
+  }
+
+  Widget _timeField(double leftMargin, double rightMargin) {
+   return Container(
+      width: 80,
+      height: 30,
+      margin: EdgeInsets.only(left: leftMargin, right: rightMargin),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+    );
+  }
+
+
 
   Widget _warningText() {
     return Container(
@@ -234,6 +287,8 @@ class _RegistrationToInstructorScreenState
           style: TextStyle(color: Colors.lightBlue),
         ));
   }
+
+
 
   Widget _continueButton() {
     return Container(
@@ -263,6 +318,8 @@ class _RegistrationToInstructorScreenState
     );
   }
 
+
+
   Widget _selectCoachButton() {
     return Container(
       width: 300.0,
@@ -277,25 +334,20 @@ class _RegistrationToInstructorScreenState
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      "выбрать определенного",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "инструктора",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold),
-                    ),
+                    _selectCoachText("выбрать определенного"),
+                    _selectCoachText("инструктора")
                   ]),
             )),
       ),
+    );
+  }
+
+  Widget _selectCoachText(String text) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
     );
   }
 }
