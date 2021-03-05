f8bdaa1a030c778f091ad269f83e40d6d00ee807
Крайнюков Сергей <skrajnjukov@rctahograf.ru>
2021-03-05 14:28:51 +0300

    reg parameters screen update

--- academ_gora/lib/screens/registration/registration_parameters_screen.dart
+++ academ_gora/lib/screens/registration/registration_parameters_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:academ_gora/screens/registration/helpers_widgets/info_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class RegistrationParametersScreen extends StatefulWidget {
   @override
@@ -6,7 +10,8 @@ class RegistrationParametersScreen extends StatefulWidget {
       _RegistrationParametersScreenState();
 }
 
-class _RegistrationParametersScreenState extends State<RegistrationParametersScreen> {
+class _RegistrationParametersScreenState
+    extends State<RegistrationParametersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,14 +22,53 @@ class _RegistrationParametersScreenState extends State<RegistrationParametersScr
               fit: BoxFit.cover,
             ),
           ),
-          child: Column(
-            children: [
-              _infoWidget()
-            ],
-          )),
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [_infoWidget()],
+              ))),
     );
   }
-  Widget _infoWidget(){
-    return Container(child: Text(""),);
+
+  Widget _infoWidget() {
+    return Container(
+        margin: EdgeInsets.only(top: 50),
+        width: 310,
+        height: 200,
+        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          image: DecorationImage(
+            image: AssetImage("assets/registration_parameters/e_1.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Container(
+                    height: 30,
+                    width: 30,
+                    margin: EdgeInsets.only(right: 15),
+                    child:
+                        Image.asset("assets/registration_parameters/e_2.png")),
+                Text(
+                  InfoText.getLevelText(),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+            Text(
+              InfoText.getText(),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              InfoText.getAge(),
+              textAlign: TextAlign.left,
+            ),
+          ],
+        ));
   }
 }
