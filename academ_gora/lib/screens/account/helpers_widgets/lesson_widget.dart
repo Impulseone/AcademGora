b21d42050cdc00fd86ba9469313413d28b5f5a1c
Крайнюков Сергей <skrajnjukov@rctahograf.ru>
2021-03-07 14:06:48 +0300

    lesson widget update

--- academ_gora/lib/screens/account/helpers_widgets/lesson_widget.dart
+++ academ_gora/lib/screens/account/helpers_widgets/lesson_widget.dart
@@ -1,3 +1,4 @@
+import 'package:academ_gora/screens/account/helpers_widgets/lesson_info.dart';
 import 'package:flutter/material.dart';
 
 class LessonWidget extends StatefulWidget {
@@ -10,33 +11,40 @@ class _LessonWidgetState extends State<LessonWidget> {
   Widget build(BuildContext context) {
     return Container(
       width: 320,
-      height: 340,
+      height: 370,
       decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage("assets/account/e2.png"),
           fit: BoxFit.cover,
         ),
       ),
-      child: Column(children: [_titleRow(), _countOfPeople(), _redactButtons()]),
+      child: Column(children: [
+        _titleRow(),
+        _countOfPeople(),
+        _redactButtons(),
+        _infoTextColumn(),
+        _instructorInfoWidget(),
+        _callToInstructorButtons()
+      ]),
     );
   }
 
   Widget _titleRow() {
     return Container(
         color: Colors.blue,
-        height: 40,
+        height: 35,
         child: Row(
           children: [
             Container(
                 margin: EdgeInsets.only(left: 20),
                 child: Text(
                   "11.02.21",
-                  style: TextStyle(color: Colors.white, fontSize: 24.0),
+                  style: TextStyle(color: Colors.white, fontSize: 22),
                 )),
             Container(
-                margin: EdgeInsets.only(left: 50),
+                margin: EdgeInsets.only(left: 70),
                 child: Text("11:30-12:30",
-                    style: TextStyle(color: Colors.white, fontSize: 24.0)))
+                    style: TextStyle(color: Colors.white, fontSize: 22)))
           ],
         ));
   }
@@ -61,19 +69,24 @@ class _LessonWidgetState extends State<LessonWidget> {
   }
 
   Widget _redactButtons() {
-    return Container(margin: EdgeInsets.only(top: 10), child: Row(
-      children: [
-        _button("РЕДАКТИРОВАТЬ", 10),
-        _button("ОТМЕНИТЬ", 80),
-      ],
-    ));
+    return Container(
+        margin: EdgeInsets.only(top: 10),
+        child: Row(
+          children: [
+            _button("РЕДАКТИРОВАТЬ", 10),
+            _button("ОТМЕНИТЬ", 80),
+          ],
+        ));
   }
 
-  Widget _button(String text, double leftMargin,) {
+  Widget _button(
+    String text,
+    double leftMargin,
+  ) {
     return GestureDetector(
       child: Container(
         alignment: Alignment.center,
-        height: 35,
+        height: 30,
         padding: EdgeInsets.all(5),
         margin: EdgeInsets.only(left: leftMargin),
         decoration: BoxDecoration(
@@ -88,5 +101,87 @@ class _LessonWidgetState extends State<LessonWidget> {
         ),
       ),
     );
+  }
+
+  Widget _infoTextColumn() {
+    return Container(
+        margin: EdgeInsets.only(top: 8, left: 10),
+        child: Column(
+          children: [
+            _infoText(LessonInfo.firstCondition()),
+            _infoText(LessonInfo.secondCondition()),
+            _infoText(LessonInfo.thirdCondition()),
+            _infoText(LessonInfo.fourthCondition()),
+            _infoText(LessonInfo.fifthCondition()),
+            _infoText(LessonInfo.sixthCondition()),
+            _infoText(LessonInfo.seventhCondition()),
+          ],
+        ));
+  }
+
+  Widget _infoText(String text) {
+    return Container(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 11),
+        ));
+  }
+
+  Widget _instructorInfoWidget() {
+    return Container(
+        margin: EdgeInsets.only(left: 10, top: 5),
+        child: Row(
+          children: [
+            Text(
+              "Инструктор:",
+              style: TextStyle(fontSize: 14),
+            ),
+            Container(
+                margin: EdgeInsets.only(left: 50),
+                child: Text(
+                  "Ярославский Александр",
+                  style: TextStyle(fontSize: 14),
+                )),
+          ],
+        ));
+  }
+
+  Widget _callToInstructorButtons() {
+    return Container(
+      margin: EdgeInsets.only(top: 5, left: 10),
+      child: Row(children: [_phoneNumberButton(), _whatsAppButton()]),
+    );
+  }
+
+  Widget _phoneNumberButton() {
+    return Container(
+        alignment: Alignment.center,
+        height: 30,
+        width: 160,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          image: DecorationImage(
+            image: AssetImage("assets/account/phone.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Text("+79999999999", style: TextStyle(color: Colors.white),));
+  }
+
+  Widget _whatsAppButton() {
+    return Container(
+        alignment: Alignment.center,
+        height: 30,
+        width: 120,
+        margin: EdgeInsets.only(left: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          image: DecorationImage(
+            image: AssetImage("assets/account/wa.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Text("Написать",style: TextStyle(color: Colors.white),));
   }
 }
